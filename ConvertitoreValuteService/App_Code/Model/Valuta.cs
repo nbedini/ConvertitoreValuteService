0708565f42867994614c129e52059b4db847283f
Nikolas Bedini <nikolasbedini04@gmail.com>
2021-10-21 14:07:37 +0200

    Aggiungere i file di progetto.

---
+++ ConvertitoreValuteService/App_Code/Model/Valuta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descrizione di riepilogo per Valuta
+/// </summary>
+public class Valuta
+{
+    public Valuta()
+    {
+        //
+        // TODO: aggiungere qui la logica del costruttore
+        //
+    }
+}
---
+++ ConvertitoreValuteService/App_Code/Service.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTA: è possibile utilizzare il comando "Rinomina" del menu "Refactoring" per modificare il nome di classe "Service" nel codice, nel file svc e nel file di configurazione contemporaneamente.
+public class Service : IService
+{
+    public string GetData(int value)
+    {
+        return string.Format("You entered: {0}", value);
+    }
+
+    public CompositeType GetDataUsingDataContract(CompositeType composite)
+    {
+        if (composite == null)
+        {
+            throw new ArgumentNullException("composite");
+        }
+        if (composite.BoolValue)
+        {
+            composite.StringValue += "Suffix";
+        }
+        return composite;
+    }
+
+
+
+}
