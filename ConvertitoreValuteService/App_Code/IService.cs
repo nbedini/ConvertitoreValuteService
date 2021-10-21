74ce3a0a4cc9ca1ffdc81620b66d82d0dfd761a2
Nikolas Bedini <nikolasbedini04@gmail.com>
2021-10-21 14:16:18 +0200

    Creazione classe Valuta

--- ConvertitoreValuteService/App_Code/IService.cs
+++ ConvertitoreValuteService/App_Code/IService.cs
@@ -14,30 +14,5 @@ public interface IService
 	[OperationContract]
 	string GetData(int value);
 
-	[OperationContract]
-	CompositeType GetDataUsingDataContract(CompositeType composite);
-
 	// TODO: aggiungere qui le operazioni del servizio
-}
-
-// Per aggiungere tipi compositi alle operazioni del servizio utilizzare un contratto di dati come descritto nell'esempio seguente.
-[DataContract]
-public class CompositeType
-{
-	bool boolValue = true;
-	string stringValue = "Hello ";
-
-	[DataMember]
-	public bool BoolValue
-	{
-		get { return boolValue; }
-		set { boolValue = value; }
-	}
-
-	[DataMember]
-	public string StringValue
-	{
-		get { return stringValue; }
-		set { stringValue = value; }
-	}
 }
--- ConvertitoreValuteService/App_Code/Model/Valuta.cs
+++ ConvertitoreValuteService/App_Code/Model/Valuta.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 
 /// <summary>
 /// Descrizione di riepilogo per Valuta
 /// </summary>
+[DataContract]
 public class Valuta
 {
-    public Valuta()
+    [DataMember]
+    public string Sigla { get; set; }
+    [DataMember]
+    public string Nome { get; set; }
+    public float TassoConversioneEuro { get; set; }
+    public Valuta(string sigla, string nome, float tassoconversioneeuro)
     {
-        //
-        // TODO: aggiungere qui la logica del costruttore
-        //
+        this.Sigla = sigla;
+        this.Nome = nome;
+        this.TassoConversioneEuro = tassoconversioneeuro;
     }
 }
